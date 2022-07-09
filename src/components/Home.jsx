195537f054c75ee5c400e27a9fc5e5f856f15f34
parag <paragpatil187@gmail.com>
2022-07-09 23:48:20 +0530

    getting data like pressure humadity sunset sunrise time for further use

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from 'react'
 // https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
 const Home = () => {
     const [data,setdata]=useState(null)
+    const[sun, setSun]=useState()
     const [search,setSearch]=useState("pune")
     useEffect(()=>{
         
@@ -12,9 +13,18 @@ const Home = () => {
         const url = `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=df2e778ac7e8e0ca500cae5a0ed91927`
         const response = await fetch(url);
         const resjson=await response.json();
+        console.log(resjson)
         setdata(resjson.main)
+        setSun(resjson.sys)
+        
 
     }
+    function msToTime(s) {
+        
+        let date = new Date(s*1000);
+        
+        return date.getHours()+":"+date.getMinutes();   
+      }
   return (
     <div>
         <div className='inputData'>
@@ -38,8 +48,20 @@ const Home = () => {
             {data.temp} cel
 
         </div>
-        <div className=''>
+        <div className='pressure'>
+           pressure {data.pressure}hpa
 
+        </div>
+        <div className='humidity'>
+            humidity{data.humidity}%
+
+        </div>
+        <div className='sunrise'>
+           sunrise {msToTime(sun.sunrise)}am
+        </div>
+        <div className='sunset'>
+           sunset {msToTime(sun.sunset)}pm
+            
         </div>
 
             </div>
