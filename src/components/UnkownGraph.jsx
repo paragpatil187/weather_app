edd9c0936aa582b8e31e1bec85201979f6c6393c
parag <paragpatil187@gmail.com>
2022-07-15 18:42:42 +0530

    data change unneeded data removed

--- src/components/UnkownGraph.jsx
+++ src/components/UnkownGraph.jsx
@@ -15,12 +15,19 @@ const UnkownGraph = ({ weather:{sunrise,sunset,timezone}}) => {
   const sunsetx=formatToLocalTime(sunset, timezone, "hh:mm a")
   //console.log("weather",weather)
 
-  const chartData = [{data:" ",val:-20}, {data:" ",val:-10},{data:sunrisex,val:0},{data:"7:00am",val:10},{data:"8:00am",val:20},{data:"9:00am",val:30},
-  {data:"10:00am",val:40},{data:"11:00am",val:50},{data:"12:00am",val:60},
-   {data:"1:00pm",val:70},
-   {data:"2:00pm",val:60},
-  {data:"3:00pm",val:50},
-  {data:"4:00pm",val:40}, {data:"5:00pm",val:30},{data:"6:00pm",val:20},{data:"7:00pm",val:10},{data:sunsetx,val:0},{data:" ",val:-10},{data:" ",val:-20}]
+  const chartData = [{data:" ",val:-20}, {data:"",val:-10},
+  {data:sunrisex,val:0},
+  {data:" ",val:10},{data:" ",val:20},{data:"",val:30},
+  {data:"",val:40},{data:"",val:50},
+  {data:"12:00am",val:60},
+   {data:" ",val:70},
+   {data:" ",val:60},
+  {data:"",val:50},
+  {data:"",val:40}, 
+  {data:" ",val:30},{data:" ",val:20},
+  {data:"",val:10},
+  {data:sunsetx,val:0},
+  {data:" ",val:-10},{data:" ",val:-20}]
   
 
   return (
@@ -36,7 +43,8 @@ const UnkownGraph = ({ weather:{sunrise,sunset,timezone}}) => {
             margin={{ top: 10, right: 30, left: 5, bottom: 30 }}
           > 
             <CartesianGrid strokeDasharray="2" />
-            <XAxis dataKey="data" interval={"preserveStartEnd"} /> 
+            <XAxis dataKey="data" interval={"preserveStartEnd"} 
+            />
             <Tooltip />
             <Legend />
             <Area
