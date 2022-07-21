391c1d410f4e72d7303f7255f48a134cc841f0be
parag <paragpatil187@gmail.com>
2022-07-21 15:50:12 +0530

    working on change value of temp by clicking

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,14 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { iconUrlFromCode } from './weatherApi'
 import "./Css/forcast.css"
+import TimeAndLocation from './TimeAndLocation'
+import Timed from './Timed'
 
 const Forecast = ({ title, items }) => {
+    const [tempday,setTempday]=useState(items[0].temp);
+    const[desc,setDesc]=useState("");
+    const[icon,setIcon]=useState("");
+
+const mydata={
+    tempday,
+    desc,
+    icon
+}
+
     console.log("items:", items)
     return (
+        <>
     <div className='forcast-second-div'>
             {items.map(data => (
                 <div key={data.title} className='forcastdiv'>
-                    <div className='seconddiv'>
+                    <div className='seconddiv' onClick={()=>{
+                        setTempday(data.temp)
+                    }}>
                     <p className='forcast-div-title'>{data.title}</p>
                     <p className='temp-div'>
                         <span>{`${data.min_temp.toFixed()}°`}</span>
@@ -21,7 +36,11 @@ const Forecast = ({ title, items }) => {
                 </div>
                 </div>
             ))}
+            
         </div>
+
+<Timed mydata={mydata}/>
+</>
 
 
     )
---
+++ src/components/Timed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Timed = ({mydata}) => {
+console.log(mydata.tempday)
+  return (
+    <div>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Timed
